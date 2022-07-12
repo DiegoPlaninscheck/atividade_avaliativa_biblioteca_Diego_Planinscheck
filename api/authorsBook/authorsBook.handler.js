5ea26d5ffecf624fbf480a0b3952c98136f8b21b
DiegoPlaninscheck <92130153+DiegoPlaninscheck@users.noreply.github.com>
2022-07-11 21:46:08 -0300

    Update authorsBook.handler.js

--- api/authorsBook/authorsBook.handler.js
+++ api/authorsBook/authorsBook.handler.js
@@ -1,25 +1,41 @@
 const crud = require("../../crud/index");
 
-function searchAuthorsBook() {
-    return crud.get("authors_Book");
+async function searchAuthorsBook() {
+  return await crud.get("authors_Book");
 }
 
 async function searchAuthorBook(id) {
-    return await crud.getByID("authors_Book", id);
+  return await crud.getByID("authors_Book", id);
 }
 
 async function createAuthorBook(data) {
-   return await crud.save("authors_Book", false, data);
+  const list = [];
+  const get = await crud.get("authors_Book");
+  list.push(get);
+  console.log(list);
+  const test = list.filter(e => {
+    console.log(e.id);
+    console.log(e.book_id);
+    if (e.author_id == data.author_id && e.book_id == data.book_id) {
+        return {"message": "equals"}
+    }
+    return e;
+  });
+  console.log(test);
 }
 
-async function editAuthorBook(data, id){
-    return await crud.save("authors_Book", id, data);
+async function editAuthorBook(data, id) {
+  return await crud.save("authors_Book", id, data);
 }
 
-async function deleteAuthorBook(id){
-    return await crud.remove("authors_Book", id);
+async function deleteAuthorBook(id) {
+  return await crud.remove("authors_Book", id);
 }
 
 module.exports = {
-    searchAuthorsBook, searchAuthorBook, createAuthorBook, editAuthorBook, deleteAuthorBook
+  searchAuthorsBook,
+  searchAuthorBook,
+  createAuthorBook,
+  editAuthorBook,
+  deleteAuthorBook,
 };
